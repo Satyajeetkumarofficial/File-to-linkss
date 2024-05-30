42c1a5dde09cb6183554697fbe6192e9fd66b107
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2024-05-30 10:43:29 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '219124813'))
-    API_HASH = str(getenv('API_HASH', 'c578b64ac7af5hak363f9e0ebfbc67923'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '5881909650:AAGTWflaXg3SDjSIhhksk5GVvxtan1onXGs'))
-    name = str(getenv('name', 'Fast_Downloader_bot'))
+    API_ID = int(getenv('API_ID', '25617967'))
+    API_HASH = str(getenv('API_HASH', '10555bea1cdfc7d2303fc13b7fd187cc'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7057265386:AAEgw175GGeExsu4iyg5TIWveIHqOzDgHAE'))
+    name = str(getenv('name', 'DemoFile2Link_Bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001977350650'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002054249965'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5313004751 866072531").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6567324192").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Rk_botowner'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'SatyajeetKumarOfficial'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -35,7 +35,7 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://mongodbuser:mogodbpass@cluster0.appjt7j.mongodb.net/'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://satyajeetkumarofficial:Guriya50@cluster0.dmmijuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Rk_botz'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))      
     SHORTLINK_URL = getenv('SHORTLINK_URL', 'onepagelink.in')
